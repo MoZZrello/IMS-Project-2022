7ea903ce866db48132256c2614778a1b5973e9ef
Richard Harman <jordan28031@gmail.com>
2022-12-01 17:16:34 +0100

    INIT

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Richard Harman on 1. 12. 2022.
+//
+
+#include "main.h"
+
---
+++ src/main.h
@@ -0,0 +1,12 @@
+//
+// Created by Richard Harman on 1. 12. 2022.
+//
+
+#ifndef IMS_PROJECT_2022_MAIN_H
+#define IMS_PROJECT_2022_MAIN_H
+
+#include "simlib.h"
+#include <getopt.h>
+#include "iostream"
+
+#endif //IMS_PROJECT_2022_MAIN_H
