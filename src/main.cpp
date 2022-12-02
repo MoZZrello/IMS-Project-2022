15c920448ff904465db46c5adce397c7ae821daa
Richard Harman <jordan28031@gmail.com>
2022-12-02 12:11:13 +0100

    Project basics

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,70 @@
 
 #include "main.h"
 
+int argParse(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    if(argParse(argc, argv) == -1){
+        return -1;
+    }
+
+    return 0;
+}
+
+int argParse(int argc, char *argv[]){
+    char* ptr;
+    long long tmpLong = 0;
+    int opt = 0;
+
+    if(argc == 2 && (strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-h") == 0)){
+        printf("Použitie: [pw] [up] [pp] [bc] [hw] [kwp] [fs] [fp] [rr] [rl] [rp] [of]\n"
+               "[pw] Sila solárneho panelu (kWp)\n"
+               "[up] Percento využitia energie (Percent) \n"
+               "[pp] Cena panelu (Kč)\n"
+               "[bc] Kapacita batérie (kWh)\n"
+               "[hw] Spotreba domu (kWh)\n"
+               "[kwp] Cena za kilowatt (Kč)\n"
+               "[fs] Pravdepodobnost chyby systému (Percent)\n"
+               "[fp] Cena opravy chyby (Kč)\n"
+               "[rr] Revízia kazdych X rokov (rok)\n"
+               "[rl] Dĺžka revízie (min)\n"
+               "[rp] Cena revízie (Kč)\n"
+               "[of] Názov súboru pre zápis výsledkov\n"
+               "- Všetky parametre sa zadávajú vo formáte FLOAT.\n");
+        return 0;
+    } else if(argc != 13) {
+        printf("Použitie: [pw] [up] [pp] [bc] [hw] [kwp] [fs] [fp] [rr] [rl] [rp] [of]\n"
+               "[pw] Sila solárneho panelu (kWp)\n"
+               "[up] Percento využitia energie (Percent) \n"
+               "[pp] Cena panelu (Kč)\n"
+               "[bc] Kapacita batérie (kWh)\n"
+               "[hw] Spotreba domu (kWh)\n"
+               "[kwp] Cena za kilowatt (Kč)\n"
+               "[fs] Pravdepodobnost chyby systému (Percent)\n"
+               "[fp] Cena opravy chyby (Kč)\n"
+               "[rr] Revízia kazdych X rokov (rok)\n"
+               "[rl] Dĺžka revízie (min)\n"
+               "[rp] Cena revízie (Kč)\n"
+               "[of] Názov súboru pre zápis výsledkov\n"
+               "- Všetky parametre sa zadávajú vo formáte FLOAT.\n");
+        fprintf(stderr, "\nNeznámy parameter\n");
+        return -1;
+    } else {
+        pw = std::stod(argv[1]);
+        up = std::stod(argv[2]);
+        pp = std::stod(argv[3]);
+        bc = std::stod(argv[4]);
+        hw = std::stod(argv[5]);
+        kwp = std::stod(argv[6]);
+        fs = std::stod(argv[7]);
+        fp = std::stod(argv[8]);
+        rr = std::stod(argv[9]);
+        rl = std::stod(argv[10]);
+        rp = std::stod(argv[11]);
+        output = argv[12];
+    }
+
+    return 0;
+}
+
--- src/main.h
+++ src/main.h
@@ -5,8 +5,53 @@
 #ifndef IMS_PROJECT_2022_MAIN_H
 #define IMS_PROJECT_2022_MAIN_H
 
-#include "simlib.h"
+#include <simlib.h>
 #include <getopt.h>
+#include <cstdio>
+#include <cstring>
 #include "iostream"
+
+#define SECOND * c_SECOND
+#define MINUTE * c_MINUTE
+#define HOUR * c_HOUR
+#define DAY * c_DAY
+#define MONTH * c_MONTH
+#define YEAR * c_YEAR
+
+const unsigned long long c_SECOND = 1;
+const unsigned long long c_MINUTE = c_SECOND * 60;
+const unsigned long long c_HOUR = c_MINUTE * 60;
+const unsigned long long c_DAY = c_HOUR * 24;
+const unsigned long long c_MONTH = c_DAY * 30;
+const unsigned long long c_YEAR = c_MONTH * 12;
+
+double pw = 0;
+double up = 100;
+double pp = 0;
+double bc = 0;
+double hw = 3000;
+double kwp = 6.5;
+double fs = 0.244;
+double fp = 5000;
+double rr = 4 YEAR;
+double rl = 30 MINUTE;
+double rp = 4000;
+
+double systemAge = 0;
+long long failureOccurance = 1 YEAR;
+long long generatePower = 1 MINUTE;
+long long generateUsage = 1 MINUTE;
+
+unsigned long long simulation_time = 25 YEAR;
+
+std::string output = "solar.dat";
+
+Store Revision("Revízor", 1);
+Store Failure("Porucha", 1);
+Store Time("Čas", 1);
+Store Usage("Spotreba", 1);
+Store Network("Sieť", 1);
+Store Battery("Batéria", (unsigned long)bc*60*1000);
+
 
 #endif //IMS_PROJECT_2022_MAIN_H
